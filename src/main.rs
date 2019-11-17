c1f349b47c834b5a43714a94c466ffb089852da4
Maxim <maxim230898@yandex.ru>
2019-11-17 10:43:02 +0400

    created onvif module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod soap;
 mod discovery;
+mod onvif;
 
 use soap::prelude::*;
 use soap::GetCapabilitiesBuilder;
@@ -7,14 +8,14 @@ use soap::GetCapabilitiesBuilder;
 fn main() {
     const XADDR: &str = "http://192.168.1.88:2000/onvif/device_service";
 
-    let xml = GetCapabilitiesBuilder::new("admin".to_string(), "admin1234".to_string()).build().unwrap();
+    // let xml = GetCapabilitiesBuilder::new("admin".to_string(), "admin1234".to_string()).build().unwrap();
 
-    let mut req = reqwest::Client::new().post(XADDR)
-        .body(xml)
-        .send()
-        .unwrap();
+    // let mut req = reqwest::Client::new().post(XADDR)
+    //     .body(xml)
+    //     .send()
+    //     .unwrap();
 
-    let res = req.text().unwrap();
+    // let res = req.text().unwrap();
 
-    println!("{}", res);
+    // println!("{}", res);
 }
