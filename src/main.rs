f759964ac45d188c5d23b1b4a4bd9d9666cd9a18
Maxim <maxim230898@yandex.ru>
2020-02-13 17:56:40 +0400

    tokio is awesome

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ use tungstenite::WebSocket;
 use std::sync::{Arc, Mutex};
 use tungstenite::Message;
 
+use tokio::sync::mpsc;
+
 fn websocket_connections(users: Arc<Mutex<Vec<WebSocket<TcpStream>>>>) {
     let server = TcpListener::bind("127.0.0.1:9001").unwrap();
 
@@ -30,55 +32,100 @@ fn websocket_connections(users: Arc<Mutex<Vec<WebSocket<TcpStream>>>>) {
     }
 }
 
-fn jpeg_reader(receiver: Receiver<Vec<u8>>, users: Arc<Mutex<Vec<WebSocket<TcpStream>>>>) {
-    loop {
-        let data = receiver.recv().unwrap();
+async fn websocket_sender(users: Arc<Mutex<Vec<WebSocket<TcpStream>>>>, mut rx: mpsc::Receiver<Vec<u8>>) {
+    println!("123");
 
-        let img = image::load_from_memory(&data).unwrap();
-
-        let dimensions = img.dimensions();
-
-        let container = img.to_bytes();
-        let pixels: Vec<&[u8]> = container.chunks(3).collect();
-        let rows: Vec<&[&[u8]]> = pixels.chunks(dimensions.0 as usize).collect();
-
-        let mut new_image = Vec::new();
-
-        for row in rows {
-            let mut new_row = Vec::new();
-            for rgb in row {
-                new_row.extend_from_slice(rgb);
-            }
-
-            new_image.append(&mut new_row.repeat(2));
-        }
-
-        let new_image: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_vec(dimensions.0 * 2, dimensions.1, new_image).unwrap();
-        let new_image = DynamicImage::ImageRgb8(new_image);
-
+    while let Some(image) = rx.recv().await {
         let mut users = users.lock().unwrap();
 
-        let mut bytes = Vec::new();
-        new_image.write_to(&mut bytes, ImageFormat::Jpeg).unwrap();
-
         for user in (*users).iter_mut() {
-            user.write_message(Message::Binary(bytes.clone())).unwrap();
+            user.write_message(Message::Binary(image.clone())).unwrap();
         }
     }
 }
 
-fn video_handler(socket: UdpSocket) {
+// fn jpeg_reader(receiver: Receiver<Vec<u8>>, users: Arc<Mutex<Vec<WebSocket<TcpStream>>>>) {
+    // loop {
+    //     let data = receiver.recv().unwrap();
+
+    //     let img = image::load_from_memory(&data).unwrap();
+
+    //     let dimensions = img.dimensions();
+
+    //     let container = img.to_bytes();
+    //     let pixels: Vec<&[u8]> = container.chunks(3).collect();
+    //     let rows: Vec<&[&[u8]]> = pixels.chunks(dimensions.0 as usize).collect();
+
+    //     let mut new_image = Vec::new();
+
+    //     for row in rows {
+    //         let mut new_row = Vec::new();
+    //         for rgb in row {
+    //             new_row.extend_from_slice(rgb);
+    //         }
+
+    //         new_image.append(&mut new_row.repeat(2));
+    //     }
+
+    //     let new_image: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_vec(dimensions.0 * 2, dimensions.1, new_image).unwrap();
+    //     let new_image = DynamicImage::ImageRgb8(new_image);
+
+    //     let mut users = users.lock().unwrap();
+
+    //     let mut bytes = Vec::new();
+    //     new_image.write_to(&mut bytes, ImageFormat::Jpeg).unwrap();
+
+        // for user in (*users).iter_mut() {
+        //     user.write_message(Message::Binary(bytes.clone())).unwrap();
+        // }
+    // }
+// }
+
+async fn process_image(data: Vec<u8>, mut tx: mpsc::Sender<Vec<u8>>) {
+    let img = image::load_from_memory(&data).unwrap();
+
+    let dimensions = img.dimensions();
+
+    let container = img.to_bytes();
+    let pixels: Vec<&[u8]> = container.chunks(3).collect();
+    let rows: Vec<&[&[u8]]> = pixels.chunks(dimensions.0 as usize).collect();
+
+    let mut new_image = Vec::new();
+
+    for row in rows {
+        let mut new_row = Vec::new();
+        for rgb in row {
+            new_row.extend_from_slice(rgb);
+        }
+
+        new_image.append(&mut new_row.repeat(2));
+    }
+
+    let new_image: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_vec(dimensions.0 * 2, dimensions.1, new_image).unwrap();
+    let new_image = DynamicImage::ImageRgb8(new_image);
+
+    let mut bytes = Vec::new();
+    new_image.write_to(&mut bytes, ImageFormat::Jpeg).unwrap();
+
+    tx.send(bytes).await.unwrap();
+}
+
+async fn video_handler(socket: UdpSocket) {
     let mut rtp_sequence = RTPSequence::new();
 
-    let (sync_sender, receiver) = sync_channel(100);
+    // let (sync_sender, receiver) = sync_channel(100);
     
     let users: Arc<Mutex<Vec<WebSocket<TcpStream>>>> = Arc::new(Mutex::new(Vec::new()));
     let users_1 = Arc::clone(&users);
     let users_2 = Arc::clone(&users);
 
-    thread::spawn(move || jpeg_reader(receiver, users_1));
+    // thread::spawn(move || jpeg_reader(receiver, users_1));
 
-    thread::spawn(move || websocket_connections(users_2));
+    let (tx, rx) = mpsc::channel(100);
+
+    thread::spawn(move || websocket_connections(users_1));
+
+    tokio::spawn(websocket_sender(users_2, rx));
 
     loop {
         let mut buf = [0; 65_535];
@@ -90,9 +137,12 @@ fn video_handler(socket: UdpSocket) {
         match rtp_sequence.push(buf) {
             Ok(status) => {
                 if let RTPSequenceStatus::LastPacket(data) = status {
-                    if let Err(_) = sync_sender.try_send(data) {
-                        println!("Buffer is full");
-                    }
+                    let tx = tx.clone();
+
+                    tokio::spawn(process_image(data, tx));
+                    // if let Err(_) = sync_sender.try_send(data) {
+                    //     println!("Buffer is full");
+                    // }
 
                     rtp_sequence.clean();
                 }
@@ -106,7 +156,8 @@ fn video_handler(socket: UdpSocket) {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut client = RTSPClient::connect("rtsp://192.168.1.88:554/av0_1".to_string()).unwrap();
 
     client.describe().unwrap();
@@ -119,7 +170,7 @@ fn main() {
     let second_socket = UdpSocket::bind(next_socket_addr).expect("Could not bind to udp socket");
 
     let cloned_main_socket = main_socket.try_clone().unwrap();
-    let video_thread = thread::spawn(move || video_handler(cloned_main_socket));
+    let video_thread = tokio::spawn(video_handler(cloned_main_socket));
 
     let cloned_second_socket = second_socket.try_clone().unwrap();
     let control_info_thread = thread::spawn(move || loop {
@@ -143,7 +194,7 @@ fn main() {
 
     client.teardown(&session).unwrap();
 
-    video_thread.join().unwrap();
+    video_thread.await.unwrap();
     control_info_thread.join().unwrap();
 
     // let mut buf = vec![];
