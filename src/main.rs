f590b0723c974fd79bacf8affd3ae1532240515a
Maxim <maxim230898@yandex.ru>
2020-03-21 15:26:02 +0400

    some code 2

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,6 @@ async fn websocket_sender(
 ) {
     println!("sending image started");
     while let Some(image) = rx.recv().await {
-        println!("sending image");
-
         let mut users = users.lock().unwrap();
 
         for user in (*users).iter_mut() {
@@ -47,56 +45,72 @@ use actix_web::{guard, web as router, App, HttpResponse, HttpServer};
 // use std::sync::Mutex;
 // use web::onvif::AuthorizedCameras;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let (sender, receiver) = mpsc::channel(100);
+fn main() -> std::io::Result<()> {
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
 
-    let mut camera = onvif::Camera::connect(
-        "http://192.168.1.88:2000/onvif/device_service".to_string(),
-        "admin".to_string(),
-        "admin1234".to_string(),
-    )
-    .await
-    .unwrap();
+    let local = tokio::task::LocalSet::new();
 
-    let users: Arc<Mutex<Vec<WebSocket<TcpStream>>>> = Arc::new(Mutex::new(Vec::new()));
-    let users_1 = Arc::clone(&users);
-    let users_2 = Arc::clone(&users);
+    local.block_on(&mut rt, async move {
+        let (sender, receiver) = mpsc::channel(100);
 
-    thread::spawn(move || websocket_connections(users_1));
+        let mut camera = onvif::Camera::connect(
+            "http://192.168.1.88:2000/onvif/device_service".to_string(),
+            "admin".to_string(),
+            "admin1234".to_string(),
+        )
+        .await
+        .unwrap();
 
-    tokio::spawn(websocket_sender(users_2, receiver));
+        let users: Arc<Mutex<Vec<WebSocket<TcpStream>>>> = Arc::new(Mutex::new(Vec::new()));
+        let users_1 = Arc::clone(&users);
+        let users_2 = Arc::clone(&users);
 
-    println!("start camera");
+        thread::spawn(move || websocket_connections(users_1));
 
-    camera.start(sender);
+        tokio::spawn(websocket_sender(users_2, receiver));
 
-    thread::sleep(Duration::from_secs(30));
+        println!("start camera");
 
-    println!("stop camera");
+        camera.start(sender);
 
-    camera.stop();
+        tokio::task::spawn_local(async move {
+            let local = tokio::task::LocalSet::new();
 
-    // let cameras_data = router::Data::new(AuthorizedCameras {
-    //     cameras: Mutex::new(HashMap::new())
-    // });
+            // let cameras_data = router::Data::new(AuthorizedCameras {
+            //     cameras: Mutex::new(HashMap::new()),
+            // });
 
-    HttpServer::new(move || {
-        App::new()
-            // .app_data(cameras_data.clone())
-            .configure(web::onvif::config)
-            // 404
-            .default_service(
-                router::resource("")
-                    .route(router::get().to(HttpResponse::NotFound))
-                    .route(
-                        router::route()
-                            .guard(guard::Not(guard::Get()))
-                            .to(HttpResponse::MethodNotAllowed),
-                    ),
-            )
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+            let sys = actix_rt::System::run_in_tokio("server", &local);
+            HttpServer::new(move || {
+                App::new()
+                    // .app_data(cameras_data.clone())
+                    .configure(web::onvif::config)
+                    // 404
+                    .default_service(
+                        router::resource("")
+                            .route(router::get().to(HttpResponse::NotFound))
+                            .route(
+                                router::route()
+                                    .guard(guard::Not(guard::Get()))
+                                    .to(HttpResponse::MethodNotAllowed),
+                            ),
+                    )
+            })
+            .bind("127.0.0.1:8080")
+            .unwrap()
+            .run()
+            .await
+            .unwrap();
+
+            sys.await.unwrap();
+        })
+        .await
+        .unwrap();
+
+        println!("stop camera");
+
+        camera.stop();
+    });
+
+    Ok(())
 }
